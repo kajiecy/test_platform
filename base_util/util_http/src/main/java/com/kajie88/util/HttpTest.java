b1214c6389e96e7089138f5f8b782d4629ee5f9b
kajiecy <851416347@qq.com>
2019-01-03 10:07:30 +0800

    爬虫已编写好 开始设计数据库

--- base_util/util_http/src/main/java/com/kajie88/util/HttpTest.java
+++ base_util/util_http/src/main/java/com/kajie88/util/HttpTest.java
@@ -11,22 +11,66 @@ import java.util.*;
  * Created by Administrator on 2019/1/2 0002.
  */
 public class HttpTest {
+    private static final String huaerjieUrl  = "https://api-prod.wallstreetcn.com/apiv1/content/lives?channel=us-stock-channel&client=pc&limit=20&first_page=true&accept=live%2Cvip-live";
+    private static final String jinshiUrl  = "https://partner-api.jin10.com/?max_id=0&count=50";
+
     public static void main(String[] args) {
-        String result = HttpUtil.sendGet("https://partner-api.jin10.com/?max_id=0&count=50","");
+//        getJinShiNews();
+        getHuaErJieNews();
+
+    }
+    public static void getHuaErJieNews(){
+        String result = HttpUtil.sendGet(huaerjieUrl,null);
+        JSONObject jsonObj = (JSONObject)JSON.parse(result);
+        JSONArray jsonArray = jsonObj.getJSONObject("data").getJSONArray("items");
+        /**
+         * 被忽略的字段 author channels
+         */
+        for (int i = 0; i < jsonArray.size(); i++) {
+            JSONObject jsonObjectItem = jsonArray.getJSONObject(i);
+            String article = jsonObjectItem.getString("article");//文章未知
+            String content = jsonObjectItem.getString("content");//新闻内容
+            String content_more = jsonObjectItem.getString("content_more");//新闻内容-更多
+            String content_text = jsonObjectItem.getString("content_text");//新闻内容-文本
+            Long display_time = jsonObjectItem.getLong("display_time");
+            String global_channel_name = jsonObjectItem.getString("global_channel_name");//频道名
+            String global_more_uri = jsonObjectItem.getString("global_more_uri");//频道名
+            String id = jsonObjectItem.getString("id");//新闻的id
+            String image_uris = jsonObjectItem.getJSONArray("image_uris").toJSONString();//展示图片集
+            Boolean is_favourite = jsonObjectItem.getBoolean("is_favourite");//是否为特别喜欢？
+            Boolean is_priced = jsonObjectItem.getBoolean("is_priced");//是否有价格？
+            String reference = jsonObjectItem.getString("reference");//参考
+            String related_themes = jsonObjectItem.getString("related_themes");//相关主题
+            Integer score = jsonObjectItem.getInteger("score");//分数？
+            JSONArray symbols = jsonObjectItem.getJSONArray("symbols");//交易品种？
+            JSONArray tags = jsonObjectItem.getJSONArray("tags");//标签
+            String title = jsonObjectItem.getString("title");//标题
+            System.out.println(jsonObjectItem);
+        }
+        System.out.println(jsonObj);
+
+    }
+    public static void getJinShiNews(){
+        String result = HttpUtil.sendGet(jinshiUrl,null);
         JSONObject jsonObj = (JSONObject)JSON.parse(result);
         JSONArray infos = jsonObj.getJSONArray("infos");
         List<String[]> jinShiDataList = new ArrayList<>();
         for (int i = 0; i < infos.size(); i++) {
             /**
              * 已知各 index 的含义 总长度为12
+             * 0
              * 1  已发现 可能性 【0，1】 1显示蓝色color #343c4c、0显示color为#e74c3c
              * 2  格式化后的时间
              * 3  消息内容
              * 4  储存转跳地址
+             * 5
              * 6  其存在图片名称 全路径为 //image.jin10.com/ + value
-             * 8 存在内容 0 但不明确含义
+             * 7
+             * 8  存在内容 0 但不明确含义
+             * 9
+             * 10
+             * 11
              * */
-
             //先想文字文字 以####0###分开
             String[] strings = infos.getString(i).split("#");
             String string0 = strings[0];
@@ -45,8 +89,6 @@ public class HttpTest {
             System.out.println(strings);
             jinShiDataList.add(strings);
         }
-
         System.out.println(jinShiDataList);
     }
-
 }
